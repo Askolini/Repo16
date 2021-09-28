32be835383e0d69d785c793a7aa35798b229c94d
Askolini <asmircc@gmail.com>
2021-09-29 00:49:47 +0200

    Inheritance

---
+++ ex04/src/index.js
@@ -0,0 +1,35 @@
+class Pets {
+    constructor(name, legs) {    
+        this.name = name;
+        this.legs = legs;
+    }
+
+
+    walk() {
+        var numberOfLegs = dog.name + " is walking on " + dog.legs + " legs";
+        return numberOfLegs;
+    }
+}
+
+class Dog extends Pets {
+    constructor(name, legs) {
+        super(name);
+        this.legs = legs;
+    }
+
+    bark() {
+        var dogSay = dog.name + " says WUF-WUF"
+        return dogSay;
+    }
+
+}
+
+let dog = new Dog("Pujdo", 4);
+
+console.log(dog.walk());
+console.log(dog.bark());
+
+module.exports = {
+    Pets,
+    Dog
+}
