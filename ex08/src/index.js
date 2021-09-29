3f261645d40534e039101e24823ba17308341d29
Askolini <asmircc@gmail.com>
2021-09-29 03:10:16 +0200

    Exporting

---
+++ ex08/src/index.js
@@ -0,0 +1,17 @@
+class Person {
+    constructor(name, email, age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    getPerson() {
+        var personInfo = "Name: " + this.name + "," + " email: " + this.email + "," + " age: " + this.age;
+        return personInfo;
+    }
+}
+
+var person = new Person("{name}", "{email}", "{age}");
+console.log(person.getPerson());
+
+module.exports = Person;
