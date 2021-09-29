0b394c538070e0cc9246b2d7cbc55bce9798b8d0
Askolini <asmircc@gmail.com>
2021-09-29 02:33:01 +0200

    Using Export

---
+++ ex06/src/index.js
@@ -0,0 +1,10 @@
+function firstName(firstName) {
+    return firstName.toUpperCase();
+}
+
+function lastName(lastName) {
+    return lastName.toLowerCase();
+}
+
+exports.firstName = firstName;
+exports.lastName = lastName;
